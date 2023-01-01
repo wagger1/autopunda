fe9994fbd52a3024b9dea4057730f8b0a136ff64
hyperbotnew <121764900+hyperbotnew@users.noreply.github.com>
2023-01-02 00:24:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,7 +63,7 @@ async def op(_, m :Message):
                 ]
             )
             add_user(m.from_user.id)
-            await("https://imgur.com/9SL3TeT", caption="**🦊 Hello {}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__Powerd By : @Hyper740**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
+           await m.reply_photo("https://imgur.com/9SL3TeT", caption="**🦊 Hello {}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__Powerd By : @Hyper740**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
     
         elif m.chat.type == enums.ChatType.GROUP or enums.ChatType.SUPERGROUP:
             keyboar = InlineKeyboardMarkup(
